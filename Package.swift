40e2619bf0a3070a340824883275aab11bb87319
gopalsorathiya <gopalsorathiya@gmail.com>
2026-04-26 12:45:41 +0530

    check new version

--- Package.swift
+++ Package.swift
@@ -9,14 +9,19 @@ let package = Package(
     products: [
         .library(
             name: "BlueForgeEngine",
-            targets: ["BlueForgeEngine"]
+            targets: ["BlueForgeEngineWrapper"]
         )
     ],
     targets: [
         .binaryTarget(
-            name: "BlueForgeEngine",
-            url: "https://github.com/gopalsorathiya/BlueForgeSDK/releases/download/0.0.2/BlueForgeEngine.xcframework.zip",
+            name: "BlueForgeBinary",
+            url: "https://github.com/gopalsorathiya/BlueForgeSDK/releases/download/0.0.3/BlueForgeEngine.xcframework.zip",
             checksum: "f746fd92bda2895368a6ee3ab872e8da9f2137f024ba99057e9b3ec16246971e"
+        ),
+        .target(
+            name: "BlueForgeEngineWrapper",
+            dependencies: ["BlueForgeBinary"],
+            path: "Sources/BlueForgeEngineWrapper"
         )
     ]
 )
