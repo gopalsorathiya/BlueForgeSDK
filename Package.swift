ee1d8f9e7bf32773f9551d881262dc8145c628c7
gopalsorathiya <gopalsorathiya@gmail.com>
2026-05-03 11:38:52 +0530

    updated build

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "NovybleEngine",
-            url: "https://github.com/gopalsorathiya/NovybleSDK/releases/download/1.1.3/NovybleEngine.xcframework.zip",
+            url: "https://github.com/gopalsorathiya/NovybleSDK/releases/download/1.1.4/NovybleEngine.xcframework.zip",
             checksum: "729a16de3a9063b1eb1ec0a25b39b684420790f34e1cf94ea4e9de03a3a90c56"
         )
     ]
